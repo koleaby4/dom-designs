dc4944d5d76b1abc03d00533231bc4a1da580f1e
Nicolai Negru <negru.n@gmail.com>
2019-05-19 11:24:50 +0100

    add cypress tests for navbar

--- cypress/integration/landing.js
+++ cypress/integration/landing.js
@@ -1,4 +1,29 @@
-it.only("content of landing page", () => {
+const selectors = {
+  servicesLink: "[data-test=services-link]",
+  projectsLink: "[data-test=projects-link]",
+  testimonialsLink: "[data-test=testimonials-link]",
+  contactsLink: "[data-test=contacts-link]",
+  hamburgerButton: "[data-test=hamburger-button]"
+};
+
+beforeEach(() => {
   cy.visit("");
-  cy.get("[data-test=services-link]").should("contain", "Services");
+});
+
+it("nav items should be visible in desktop mode", () => {
+  cy.get(selectors.servicesLink).should("contain", "Services");
+  cy.get(selectors.projectsLink).should("contain", "Projects");
+  cy.get(selectors.testimonialsLink).should("contain", "Testimonials");
+  cy.get(selectors.contactsLink).should("contain", "Contacts");
+  cy.get(selectors.hamburgerButton).should("not.be.visible");
+});
+
+it("nav items should be hidden in hamburger in mobile mode", () => {
+  cy.viewport("iphone-6");
+
+  cy.get(selectors.hamburgerButton).should("be.visible");
+  cy.get(selectors.servicesLink).should("not.be.visible");
+  cy.get(selectors.projectsLink).should("not.be.visible");
+  cy.get(selectors.testimonialsLink).should("not.be.visible");
+  cy.get(selectors.contactsLink).should("not.be.visible");
 });
