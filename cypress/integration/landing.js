208e7c21d34537c322dd22a63c43053c8b71db48
Nicolai Negru <negru.n@gmail.com>
2019-05-19 11:31:20 +0100

    expand cypress test for landing page in mobile view

--- cypress/integration/landing.js
+++ cypress/integration/landing.js
@@ -3,7 +3,8 @@ const selectors = {
   projectsLink: "[data-test=projects-link]",
   testimonialsLink: "[data-test=testimonials-link]",
   contactsLink: "[data-test=contacts-link]",
-  hamburgerButton: "[data-test=hamburger-button]"
+  hamburgerButton: "[data-test=hamburger-button]",
+  ourProjectsButton: "[data-test=our-projects-CTA]"
 };
 
 beforeEach(() => {
@@ -15,15 +16,26 @@ it("nav items should be visible in desktop mode", () => {
   cy.get(selectors.projectsLink).should("contain", "Projects");
   cy.get(selectors.testimonialsLink).should("contain", "Testimonials");
   cy.get(selectors.contactsLink).should("contain", "Contacts");
+  cy.get(selectors.ourProjectsButton).should("contain", "Our projects");
   cy.get(selectors.hamburgerButton).should("not.be.visible");
 });
 
-it("nav items should be hidden in hamburger in mobile mode", () => {
+it("nav items should appear when hamburger is clicked from mobile mode", () => {
   cy.viewport("iphone-6");
 
   cy.get(selectors.hamburgerButton).should("be.visible");
+
   cy.get(selectors.servicesLink).should("not.be.visible");
   cy.get(selectors.projectsLink).should("not.be.visible");
   cy.get(selectors.testimonialsLink).should("not.be.visible");
   cy.get(selectors.contactsLink).should("not.be.visible");
+
+  // after expanding hamburger - nav items are shown
+  cy.get(selectors.hamburgerButton).click();
+  cy.get(selectors.servicesLink).should("be.visible");
+  cy.get(selectors.projectsLink).should("be.visible");
+  cy.get(selectors.testimonialsLink).should("be.visible");
+  cy.get(selectors.contactsLink).should("be.visible");
+
+  cy.get(selectors.ourProjectsButton).should("contain", "Our projects");
 });
